5044579794006b47b7fc4e5c4ff6535fac225e2f
Ale <alexxandro.v2@gmail.com>
2017-06-21 12:07:33 +0200

    hybrid model fixed

--- source/CrossValidation.py
+++ source/CrossValidation.py
@@ -6,32 +6,31 @@ from DataParser import parse_data
 
 class CrossValidator:
     # data and targets file-paths, k of k-fold, choose among ann or HMM, array of value for ann: eta and runs
-    def __init__(self, data_fp, targets_fp, k, ann_train):
-        ds = parse_data(data_fp, targets_fp)
+    def __init__(self, data_fp, targets_fp, k):
+        #ds = parse_data(data_fp, targets_fp)
+        #ds.to_csv(path_or_buf='../df.csv')
+        ds = pd.read_csv('../df.csv', header=0, sep=';')  # TODO ripristinare il vecchio parser
         subset_size = np.math.ceil(len(ds) / k)  # size of test set for the k-fold cross val.
-        results = pd.Series([])  # contains the percentage of matched prediction for each fold
-        state_frequency = pd.Series.sort_index(ds.ix[:, ds.shape[1] - 1].value_counts())
+        results = []  # contains the percentage of non-matched prediction for each fold
         for i in range(k):
             test_set = ds.ix[i * subset_size:][:subset_size].reset_index(drop=True)
-            valid_set = ds.ix[(i+1) * subset_size:][:subset_size].reset_index(drop=True)
-            if i==0:
+            valid_set = ds.ix[(i + 1) * subset_size:][:subset_size].reset_index(drop=True)
+            if i == 0:
                 training_set = ds.ix[(i + 2) * subset_size:].reset_index(drop=True)
             else:
-                training_set = pd.concat([ds.ix[:i * subset_size], ds.ix[(i + 2) * subset_size:]]).reset_index(drop=True)
+                training_set = pd.concat([ds.ix[:i * subset_size], ds.ix[(i + 2) * subset_size:]]).reset_index(
+                    drop=True)
             # hybrid model's initialization
-            model = HybridHMM(training_set, state_frequency)
-            err = 1
+            model = HybridHMM(training_set)
             # training and testing of the fold
-            while err > 0.4:
-                model.train(ann_train[0], ann_train[1])
-                err = model.test(valid_set)
-                print('Error of hybrid model: ', err)
-            results.append(pd.Series([model.test(test_set)]))
-        print(results)
+            err = model.test(valid_set)
+            print('Fold: ', i, 'error on validation: ', err)
+            print('Fold: ', i, 'error on test: ', model.test(test_set))
+            results.append(err)
 
 
 def execute_my_hmm():
-    cv = CrossValidator('../A_Sensors.txt', '../A_ADLs.txt', 10, [0.03, 10000])
+    cv = CrossValidator('../A_Sensors.txt', '../A_ADLs.txt', 10)
 
 
 execute_my_hmm()
--- source/HybridHMM.py
+++ source/HybridHMM.py
@@ -1,20 +1,13 @@
+import tensorflow as tf
+sess = tf.Session()
+from keras import backend as k
+k.set_session(sess)
+from keras.models import Sequential
+from keras.layers import Dense
 import numpy as np
 import pandas as pd
-from hmmlearn import hmm
-import math
-from sklearn import preprocessing, metrics
-from NeuralNetwork import NeuralNetwork
+from sklearn import preprocessing
 from TransitionMatrixGen import build_transmat
-
-
-def bayes_rule(out, t_priors, f_priors):
-    scaled_likelihood = np.divide(out, t_priors)
-    obsmatrix_ = np.zeros((t_priors.shape[0], f_priors.shape[1]))
-    for col in range(f_priors.shape[1]):
-        feature_prior = f_priors[0][col]
-        obsmatrix_[:, col] = np.multiply(scaled_likelihood, feature_prior)
-        obsmatrix_ = preprocessing.normalize(obsmatrix_, norm='l1')
-    return obsmatrix_
 
 
 def convert_targets(self, targets):
@@ -28,7 +21,7 @@ def convert_targets(self, targets):
 
 def encode_features_targets(ds):
     states_count = pd.Series.sort_index(ds.ix[:, ds.shape[1] - 1].value_counts()).shape[0]
-    features = ds.ix[:, :ds.shape[1] - 2]
+    features = ds.ix[:, :ds.shape[1] - 1]  # TODO mettere -2 quando torno a usare il dataparser
     encoded_targets = np.zeros((ds.shape[0], states_count))
     row = 0
     for t_value in ds.ix[:, ds.shape[1] - 1]:
@@ -36,103 +29,114 @@ def encode_features_targets(ds):
         row = row + 1
     return features, encoded_targets
 
+# input: ann output and target w.r.t. 1 single example
+def err_rate(pred, targets):
+    err = 0
+    l = len(pred)
+    for i in range(l):
+        err += not (pred[i] == targets[i])
+    return err / l
+
+
 
 class HybridHMM:
     # ds: pandas DataFrame that contains the ds (training set),
     # state_frequency: pandas Series that contains the frequency which they appear with
-    def __init__(self, ds, state_frequency):
-        self.ds = ds
-        self.state_frequency = state_frequency
-        self.states_count = state_frequency.shape[0]
-        f_priors = ds[ds >= 1.0].count()  # prior probabilities of features
-        # normalize features' prior probabilities
-        reshape_for_norm = f_priors.ix[:f_priors.shape[0]-2].values.reshape(1,-1)
-        self.f_priors = preprocessing.normalize(reshape_for_norm, norm='l1')
-        self.pi = np.divide(state_frequency, ds.shape[0])
-        self.o = np.zeros([self.states_count, ds.shape[1]-1])  # initialized to zero
-        self.t = build_transmat(ds, self.states_count)
-        # ANN initialization
-        self.ann = NeuralNetwork(ds.shape[1]-1, self.states_count)
-
-    def train(self, learning_rate, runs):
+    def __init__(self, ds, ):
         # separating features from targets
         # building one-hot encoded targets
-        features, targets = encode_features_targets(self.ds)
-        # training ANN
-        acc = []
-        div = 20
-        #a = self.ann.train(features, targets)
-        d = self.ann.train(features.ix[136:456, :], targets[136:457, :])
-        evs = []
-        for i in range(136, 160, 1):
-            pred = self.ann.get_prediction([features.ix[i, :]])[0]
-            target_pred = targets[i, :]
-            print(targets[i,:], pred)
-            evs.append(self.evaluate(pred, target_pred))
-        print(evs)
-        '''
-        for batch_run in range(0, features.shape[0], int(features.shape[0]/div)):
-            ac_acc = (self.ann.train(features[batch_run:][:int(features.shape[0]/div)],
-                           targets[batch_run:][:int(features.shape[0]/div)]))
-            acc.append(ac_acc)
-            if batch_run > features.shape[0]-(2*(features.shape[0]/div)):
-                m = np.mean(acc)
-                if m < 0.8:
-                    batch_run = 0
-                print('Average accuracy during NN training: ', m)
-        '''
-    def test(self, test_set):
-        features, targets = encode_features_targets(test_set)
-        features = features.reset_index(drop=True)
-        features = features.as_matrix()
-        # compute bayes for each time slice
+        features, targets = encode_features_targets(ds)
+        self.labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+        self.f_priors = features.groupby(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']).size().reset_index(name='F_Priors')
+        self.f_frequencies = self.f_priors['F_Priors'].divide(features.shape[0])
+        del self.f_priors['F_Priors']
+        self.ds = ds
+        self.state_frequency =  pd.Series.sort_index(ds.ix[:, ds.shape[1] - 1].value_counts())
+        self.states_count = self.state_frequency.shape[0]
         # priors prob. for hidden states
-        t_priors = np.divide(self.state_frequency, self.ds.shape[0])
-        t_priors = t_priors.as_matrix()
-        self.pi = t_priors
-        result = self.viterbi(np.arange(self.states_count), features[67:77])
-        evaluation = self.evaluate(result, self.ds.reset_index(drop=True).ix[67:77, self.ds.shape[1]-1].as_matrix())
-        return evaluation
+        self.pi = np.divide(self.state_frequency, ds.shape[0]).as_matrix()
+        self.o = np.zeros([self.states_count, self.f_priors.shape[0]])  # initialized to zero
+        self.t = build_transmat(ds, self.states_count)
+        # ANN initialization and training
+        self.ann = self.train(features, targets)
 
-    # o_space: observations' space, s_space: states' space, seq: obs. sequence
-    # all input must be np.array
-    # output most likely sequence
-    def viterbi(self, s_space, seq):
-        T1 = np.zeros((s_space.shape[0], seq.shape[0]))
-        T2 = np.zeros((s_space.shape[0], seq.shape[0]))
-        for t_i in range(seq.shape[0]):
-            print('features: ', seq[t_i], 'predizione: ', self.ann.get_prediction([seq[t_i]]))
-            for s in range(s_space.shape[0]):
-                # o gets updated at each time slice:
-                updated_o = bayes_rule(self.ann.get_prediction([seq[t_i]]), self.pi, self.f_priors)
-                obs_indices = np.nonzero(seq[0])
-                mul_obs_prob = 1
-                for i in obs_indices[0]:  # t_i are independent: their joint probs
-                    mul_obs_prob *= updated_o[s, i]   # are simple given by the multiplication of each one prob
-                if t_i == 0:
-                    T1[s, 0] = self.pi[s] * mul_obs_prob
-                    T2[s, 0] = 0
-                else:
-                    pw_multiplied = np.multiply(T1[:, t_i-1], self.t[:, s])  # point-wise multiply
-                    max_trans_prob = pw_multiplied[pw_multiplied.argmax()]
-                    T1[s, t_i] = mul_obs_prob * max_trans_prob
-                    T2[s, t_i] = max_trans_prob
-        z = np.zeros((T1.shape[1]))
-        for t in range(T1.shape[1]):
-            z[t] = np.argmax(T1[:, t])
-        result = np.zeros(z.shape[0])
-        for t_z in range(z.shape[0]):
-            result[t_z] = z[t_z]  # z contains states indices
-        for i in range(z.shape[0]-1, 1, 1):
-            z[i-1] = T2[z[i], i]
-            result[i-1] = s[z[i-1]]
-        return result
+    def train(self, features, targets):
+        s = features.shape[0]
+        k = s / 10
+        X_train = features.ix[:int(s - k)].reset_index(drop=True).as_matrix()
+        Y_train = targets[:int(targets.shape[0] - k + 1)]
+        X_test = features.ix[int(s - k):].reset_index(drop=True).as_matrix()
+        Y_test = targets[int(targets.shape[0] - k):]
+        model = Sequential()
+        model.add(Dense(12, activation='relu', input_shape=(12,)))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(9, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+        model.fit(X_train, Y_train,
+                  batch_size=1332, epochs=10, verbose=0)
+        score = model.evaluate(X_test, Y_test, verbose=0)
+        return model
 
-    # input: ann output and target w.r.t. 1 single example
-    @staticmethod
-    def evaluate(pred, targets):
-        err = 0
-        for i in range(pred.shape[0]):
-            err += not(pred[i] == targets[i])
-        return err / pred.shape[0]
+    def get_prediction(self, x):
+        return self.ann.predict(x)
 
+    def test(self, test_set):
+        features = test_set.ix[:, :test_set.shape[1] - 1].reset_index(drop=True).as_matrix()
+        targets = test_set.ix[:, test_set.shape[1]-1].reset_index(drop=True).as_matrix()
+        error_rates = []
+        day = 60 *24  # express in segments
+        for t in range(0, features.shape[0]-day, day):  # 24 * 60 = day duration in minutes
+            hybrid_prediction = self.decode(features[t:(t+day), :])
+            error_rates.append(err_rate(hybrid_prediction, targets[t:(t+day)]))
+        error_rate_mean = np.mean(error_rates)
+        return error_rate_mean
+
+    # input: sequence of observations
+    def decode(self, obs_seq):
+        viterbi = np.zeros((self.states_count, len(obs_seq)))
+        back_pointer = np.ones((self.states_count, len(obs_seq)), 'int32') * -1
+        # initialization
+        out = self.update_obs_mat(obs_seq[0])
+        viterbi[:, 0] = np.squeeze(out.T)
+        for t in range(1, len(obs_seq)):
+            out = self.update_obs_mat(obs_seq[0])
+            viterbi[:, t] = (viterbi[:, t - 1, None].dot(out) * self.t).max(0)
+            back_pointer[:, t] = (np.tile(viterbi[:, t - 1, None], [1, self.states_count]) * self.t).argmax(0)
+        # termination
+        tokens = [viterbi[:, -1].argmax()]
+        for i in range(len(obs_seq) - 1, 0, -1):
+            tokens.append(back_pointer[tokens[-1], i])
+        return tokens[::-1]
+
+    # input: configuration, array of size equal to number of features
+    def config_index(self, config):
+        for i in self.f_priors.iterrows():
+            if (i[1] == config).all():
+                return i[0]
+        return None
+
+    # input: list of active features at a given time,
+    def access_obs(self, obs):
+        active_f = np.array(np.nonzero(obs), dtype=int)
+        mul = np.ones((self.states_count,))
+        for o in active_f[0]:
+            mul = np.multiply(mul, self.o[:, o])
+        mul = np.transpose([mul])
+        return mul
+
+    # input: observation at a given time
+    def update_obs_mat(self, obs):
+        ann_pred = self.get_prediction(np.atleast_2d([obs]))
+        return ann_pred
+
+    def bayes_rule(self, out, t_priors, observed_conf):
+        out = np.divide(out, t_priors)
+        obsmatrix_ = np.zeros((t_priors.shape[0], self.f_priors.shape[0]))
+        for col in range(self.f_frequencies.shape[0]):
+            obsmatrix_[:, col] = np.multiply(out, self.f_frequencies.ix[col])
+        obsmatrix_ = preprocessing.normalize(obsmatrix_, norm='l1')
+        for r in obsmatrix_:
+            print(np.sum(r))
+        return obsmatrix_
