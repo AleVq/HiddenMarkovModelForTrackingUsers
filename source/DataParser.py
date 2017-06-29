9def151e3781609d66aea6f475e459535562630c
Ale <alexxandro.v2@gmail.com>
2017-06-29 10:09:52 +0200

    Prototype functioning on both dataset A and B

--- source/DataParser.py
+++ source/DataParser.py
@@ -7,14 +7,14 @@ def parse_data(data_fp, targets_fp):
     ds = pd.read_csv(data_fp, sep='\s+', header=0, parse_dates=[['Start', 'time'], ['End', 'time.1']])
     target_activities = pd.read_csv(targets_fp, sep='\s+',
                                     header=0, parse_dates=[['Start', 'time'], ['End', 'time.1']])
-    features = pd.Series(ds.ix[:, 2].unique())
-    states = pd.Series(target_activities.ix[:, -1].unique())
+    features = ds[['Location', 'Place']].drop_duplicates().reset_index(drop=True)
+    states = pd.Series(target_activities.ix[:, -1].unique())  # identify activities
     end = target_activities.ix[target_activities.shape[0]-1, 1]
     start = target_activities.ix[0, 0]
     # interval vars
     delta = np.timedelta64(60, 's')
-    num_of_time_segments = np.math.ceil((end - start) / delta)
-    ann_dataset = np.empty((0, features.shape[0]+1), int)
+    # ann_dataset = np.empty((0, features.shape[0]+1), int)
+    ann_dataset = []
     ds_counter = 0
     for act in target_activities.iterrows():
         # iterate over t from act's start time to act's end time, stepping with delta
@@ -25,12 +25,16 @@ def parse_data(data_fp, targets_fp):
             temp_count = ds_counter
             while not(ds.ix[temp_count]['Start_time'] >= (t + delta)):
                 if max(t, ds.ix[temp_count]['Start_time']) <= min((t + delta), ds.ix[temp_count]['End_time.1']):
-                    active_sensors[features[features == ds.ix[temp_count]['Location']].index] = 1
+                    active_sensors[features[(features['Location'] == ds.ix[temp_count]['Location']) \
+                                        & (features['Place'] == ds.ix[temp_count]['Place'])].index] = 1
                 temp_count += 1
+                if temp_count >= ds.shape[0]:
+                    break
             if ds.ix[ds_counter]['End_time.1'] <= t:
                 ds_counter += 1
             new_row = np.concatenate([active_sensors, mapped_act], axis=0)
-            ann_dataset = np.append(ann_dataset, [new_row], axis=0)
+            # ann_dataset = np.append(ann_dataset, [new_row], axis=0)
+            ann_dataset.append(new_row)
             t = t + delta
         if 0 == (act[0] % 50):
             print(act[0], '...')
@@ -39,4 +43,4 @@ def parse_data(data_fp, targets_fp):
     non_null_features= (ann_dataset.ix[:, :(ann_dataset.shape[1]-2)].T != 0).any()
     ann_dataset = ann_dataset.loc[non_null_features].reset_index(drop=True)
     print('Data parsed.')
-    return pd.DataFrame(ann_dataset)
+    return ann_dataset
